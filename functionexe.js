a6a700776c2f78f2fc5fc781ddabe43bebda97a3
Bibi <84073518+xBibie@users.noreply.github.com>
2021-07-02 14:51:33 +0200

    Add files via upload

---
+++ functionexe.js
@@ -0,0 +1,20 @@
+
+const adult = function(age) {
+    if (age >= 18); {
+        console.log('false');
+    } else {
+        console.log('true');
+    } 
+};
+
+const adult2 = function(age2) {
+   if ('false');{
+    return: ('Hello there');
+}else{ 
+    return: ('Hi kiddo');
+}
+};
+
+console.log (result 'adult2')
+
+
